7c662e402b7c9689368d1a2d9199a549299a7c83
unknown <mikecarleson@example.com>
2019-06-13 05:35:08 -0700

    heroku commit

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://mikecarleson:Tavern07@beer-app-r4lpu.mongodb.net/test?retryWrites=true&w=majority");
+
+// Add routes, both API and view
+app.use(routes);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3001;
 
 // Start the API server
 app.listen(PORT, function() {
