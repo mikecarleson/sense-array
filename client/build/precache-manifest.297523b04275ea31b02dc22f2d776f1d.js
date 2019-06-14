06f29269fc23e557a719467843ea16494fd1c583
unknown <mikecarleson@example.com>
2019-06-13 17:20:10 -0700

    build

---
+++ client/build/precache-manifest.297523b04275ea31b02dc22f2d776f1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d4167aad2003e3cdc35",
+    "url": "/static/js/main.c3eedce0.chunk.js"
+  },
+  {
+    "revision": "fefee7239ceebdc7d209",
+    "url": "/static/js/2.d670e194.chunk.js"
+  },
+  {
+    "revision": "1d4167aad2003e3cdc35",
+    "url": "/static/css/main.468acd8d.chunk.css"
+  },
+  {
+    "revision": "9684a8bfab84031cb90b7283d3f9a521",
+    "url": "/index.html"
+  }
+];
